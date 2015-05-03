b31c5eaded91fcbea3bd072fc5879d285066b67d
Rémi Blaise <remi.blaise@gmx.fr>
2015-05-03 17:42:48 +0200

    [Index] Add an animated arrow to scroll down

---
+++ Resources/public/js/dynamic.js
@@ -0,0 +1,14 @@
+jQuery(function($){
+	animated_arrow = $('#animated_arrow');
+	w = $(window);
+	
+	animated_arrow.click(function(){
+		event.preventDefault();
+		var n = w.height();
+		$('html, body').animate({ scrollTop: n }, 1000);
+	});
+	
+	w.scroll(function(){
+		animated_arrow.css('display', 'none');
+	});
+});

8b0704ea2b339b66fb774e60bb752c49847585d8
Rémi Blaise <remi.blaise@gmx.fr>
2018-03-20 17:37:58 +0100

    Add renov page

--- src/Zz/ChezZzortellBundle/Controller/IndexController.php
+++ src/Zz/ChezZzortellBundle/Controller/IndexController.php
@@ -8,18 +8,20 @@ class IndexController extends Controller
 {
     public function freelanceAction()
     {
-        return $this->render('ZzChezZzortellBundle:Index:freelance.html.twig');
+        return $this->render('ZzChezZzortellBundle::renov.html.twig');
+        // return $this->render('ZzChezZzortellBundle:Index:freelance.html.twig');
     }
 
     public function portfolioAction()
     {
-        return $this->render('ZzChezZzortellBundle:Index:portfolio.html.twig');
+        return $this->render('ZzChezZzortellBundle::renov.html.twig');
+        // return $this->render('ZzChezZzortellBundle:Index:portfolio.html.twig');
     }
 
-    public function indexAction()
-    {
-        return $this->render('ZzChezZzortellBundle:Index:index.html.twig');
-    }
+    // public function indexAction()
+    // {
+    //     return $this->render('ZzChezZzortellBundle:Index:index.html.twig');
+    // }
 
     public function librariesAction()
     {
